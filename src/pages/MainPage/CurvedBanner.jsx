236481b4274c37aa6ceb8f04adc3e098dc55fef4
nayeong lee <lny021102@gmail.com>
2024-01-30 17:06:55 +0900

    [ADD] function banner 완성

--- src/pages/MainPage/CurvedBanner.jsx
+++ src/pages/MainPage/CurvedBanner.jsx
@@ -9,14 +9,9 @@ const MainContainer = styled.div`
     gap: 10px;
 `;
 
-const Container = styled.div`
-width: 100%;
-height: 700px;
-`;
-
 const CurvedContainer = styled.div`
   width: 100%;
-  height: 300px;
+  height: 450px;
   background-image: linear-gradient(to right, #E797B8, #8B5EFF, #1774D0);
   position: relative;
   z-index: 0;
@@ -33,10 +28,10 @@ const Curved = styled.div`
 
 const Curved_2 = styled.div`
   width: 100%;
-  top: 400px;
+  top: 350px;
   height: 100px;
   background-color: black;
-  clip-path: ellipse(50% 100% at 50% 0%);
+  clip-path: ellipse(50% 100% at 50% 100%);
   position: absolute;
   z-index: 1;
 `;
@@ -47,7 +42,7 @@ export default function Component() {
 
             <CurvedContainer>
                 <Curved />
-
+                <Curved_2 />
             </CurvedContainer>
 
         </MainContainer>
--- src/pages/MainPage/FunctionBanner.jsx
+++ src/pages/MainPage/FunctionBanner.jsx
@@ -15,19 +15,13 @@ const Title = styled.h1`
 font-size: 48px;
 font-weight: bold;
 color: white;
-margin-bottom: 10px;
+margin: 10px;
 `;
 
 const Detail = styled.a`
 font-size: 18px;
 color: white;
-`;
-
-const TextContainer = styled.div`
-display: flex;
-flex-direction: column;
-text-align: left;
-margin-bottom: 50px;
+margin: 10px;
 `;
 
 const FunctionContainer = styled.div`
@@ -38,7 +32,26 @@ flex-direction: column;
 const Function = styled.div`
 display: flex;
 flex-direction: row;
+border-radius: 15px;
+background-color: #222228;
 gap: 20px;
+padding: 25px;
+margin: 10px;
+justify-content: space-between;
+`;
+
+const CustomRow = styled.div`
+display: flex;
+flex-direction: row;
+gap: 10px;
+justify-content: flex-start;
+`;
+
+const CustomRow2 = styled.div`
+display: flex;
+flex-direction: row;
+gap: 10px;
+justify-content: flex-end;
 `;
 
 const ImgContainer = styled.div`
@@ -46,9 +59,22 @@ display: flex;
 flex-direction: column;
 border-radius: 10px;
 border: none;
+padding: 10px;
+`;
+
+const ImgText1 = styled.a`
 color: white;
 font-size: 16px;
-padding: 10px;
+line-height: 20px;
+margin-top: 5px;
+`;
+
+const ImgText2 = styled.a`
+color: white;
+font-size: 16px;
+line-height: 20px;
+margin-top: 5px;
+text-align: right;
 `;
 
 const FunctionTitle = styled.h1`
@@ -65,32 +91,93 @@ color: white;
 margin-bottom: 10px;
 `;
 
+const FunctionTitle2 = styled.h1`
+font-size: 48px;
+font-weight: bold;
+color: #FB969C;
+margin-bottom: 10px;
+text-align: right;
+`;
+
+const SemiTitle2 = styled.h1`
+font-size: 40px;
+font-weight: bold;
+color: white;
+margin-bottom: 10px;
+text-align: right;
+`;
+
+const Sizedbox = styled.div`
+width: 5px;
+height: 30px;
+`;
+
+const Line = styled.div`
+width: ${props => props.width || '100px'};
+height: 1px;
+background-color: #F0F0F0;
+margin: 10px;
+`;
+
+const AnButton = styled.button`
+background-color: #8B5EFF;
+border-radius: 15px;
+color: white;
+font-weight: bold;
+font-size: 20px;
+padding: 10px;
+width: 280px;
+height: 60px;
+border: none;
+`;
+
+const CheckButton = styled.button`
+background-color: #122033;
+border-radius: 15px;
+color: white;
+font-weight: bold;
+font-size: 20px;
+padding: 10px;
+width: 280px;
+height: 60px;
+border: none;
+`;
+
 export default function Component() {
     return (
         <MainContainer>
-            <TextContainer>
+            <FunctionContainer>
                 <Title>기능소개</Title>
                 <Detail>법률 문서를 다룰 수 있는 닥터로우만의 독특한 서비스를 알려드립니다.</Detail>
-            </TextContainer>
-            <FunctionContainer>
+                <Sizedbox />
                 <Function>
                     <ContainerColumn>
                         <FunctionTitle>계약 내용 분석</FunctionTitle>
                         <SemiTitle>이미지, PDF 일괄 지원</SemiTitle>
+                        <Line width='560px' />
+                        <CustomRow>
+                            <AnButton>계약 내용 분석 바로가기</AnButton>
+                            <CheckButton>내 분석 기록 확인하기</CheckButton>
+                        </CustomRow>
                     </ContainerColumn>
                     <ImgContainer>
-                        <StyledImg src={'img/main_function_1.png'} width='100px' height='100px' />
-                        위험한 조항이 있는지 확인하고<br />내용을 이해하기 쉽게 요약해드립니다
+                        <StyledImg src={'img/main_function_1.png'} width='300px' height='100px' />
+                        <ImgText2>위험한 조항이 있는지 확인하고<br />내용을 이해하기 쉽게 요약해드립니다</ImgText2>
                     </ImgContainer>
                 </Function>
+                <Sizedbox />
                 <Function>
                     <ImgContainer>
-                        <StyledImg src={'img/main_function_1.png'} width='100px' height='100px' />
-                        위험한 조항이 있는지 확인하고<br />내용을 이해하기 쉽게 요약해드립니다
+                        <StyledImg src={'img/main_function_2.png'} width='300px' height='100px' />
+                        <ImgText1>위험한 조항이 있는지 확인하고<br />내용을 이해하기 쉽게 요약해드립니다</ImgText1>
                     </ImgContainer>
                     <ContainerColumn>
-                        <FunctionTitle>계약 내용 분석</FunctionTitle>
-                        <SemiTitle>이미지, PDF 일괄 지원</SemiTitle>
+                        <FunctionTitle2>계약 문서 생성</FunctionTitle2>
+                        <SemiTitle2>철저한 가이드라인으로 간단한 설계</SemiTitle2>
+                        <Line width='630px' />
+                        <CustomRow2>
+                            <AnButton>문서 자동생성 바로가기</AnButton>
+                        </CustomRow2>
                     </ContainerColumn>
                 </Function>
             </FunctionContainer>
